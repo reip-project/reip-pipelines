0d00b036c5d57932cf84ce058a0801bff085457d
Bea Steers <bsteers@nyu.edu>
2020-08-26 15:02:05 -0400

    add token class

--- reip/constants.py
+++ reip/constants.py
@@ -8,9 +8,36 @@
 # gets pickled.
 make_token = lambda x: f'|(((( reip {x.upper()} ))))|'
 
+# __SPLIT = '@#$%^&*()'
+# _TOKEN_START, _TOKEN_END = make_token(__SPLIT).split(__SPLIT)
+# is_token = lambda x: isinstance(x, str) and x.startswith(_TOKEN_START) and x.endswith(_TOKEN_END)
+
+class Token:
+    def __init__(self, name):
+        self.name = name.upper()
+        self.id = make_token(name)
+
+    def __repr__(self):
+        return self.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        return isinstance(other, Token) and self.id == other.id
+
+
+# Use this to signify a blank output. If you have a block that outputs
+# [0, 1, BLANK, 2, 3], the next block would receive process(1, 2, 3, 4)
+BLANK = Token('BLANK')
 # return this from a block and the sources won't be incremented.
-RETRY = make_token('RETRY')
-CLOSE = make_token('CLOSE')
-TERMINATE = make_token('TERMINATE')
+RETRY = Token('RETRY')
+NEXT = Token('NEXT')
+# if a block returns these, it will close/terminate itself and will
+# cascade down to other blocks.
+CLOSE = Token('CLOSE')
+TERMINATE = Token('TERMINATE')
+
+SOURCE_TOKENS = {RETRY, NEXT}
 
 MIN_SLEEP = 1e-6
