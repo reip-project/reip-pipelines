9c4b4cf6ca9a260c3ef6a76a9181f57269f91f1b
Bea Steers <bsteers@nyu.edu>
2020-09-16 15:27:37 -0400

    don't needlessly unserialize data

--- reip/interface.py
+++ reip/interface.py
@@ -3,7 +3,7 @@ import time
 
 def skip_strategy(source):
     while not source.empty() and source._skip_id < source.skip:
-        source._get()
+        # source._get()
         source.next()  # discard the buffer
         source._skip_id += 1
         source.skipped += 1
@@ -16,7 +16,7 @@ def skip_strategy(source):
 
 def latest_strategy(source):
     while not source.last():
-        source._get()
+        # source._get()
         source.next()  # discard the buffer
         source.skipped += 1
     return source._get()
