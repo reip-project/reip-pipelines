0c363c4c193c9d9aea31d970ac65ff29d01154c1
Bea Steers <bsteers@nyu.edu>
2020-09-21 14:32:09 -0400

    add parents prop, graph+task bug fix, add parent test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -22,16 +22,13 @@ class Indexes:  # help visualize graph inheritance depth
 idxs = Indexes()
 
 @contextmanager
-def checked_graph_instance(cls, parent, **kw):
-    print(11111, parent.name)
-    print(22222, [b.name for b in parent.blocks], parent.name)
+def checked_graph_instance(cls, parent, parents=None, task_id=None, **kw):
     with cls(name=idxs.get(cls.__name__)) as g1:
         assert isinstance(g1, cls)
+        assert task_id is False or g1.task_id == task_id
         assert reip.default_graph() is g1
         assert g1 is not reip.top_graph()
-        print(33333, [b.name for b in parent.blocks])
-        print(44444, parent.parent_id, parent.task_id, g1.parent_id, g1.task_id)
-        print()
+        assert parents is None or [p.name for p in parents] == [p.name for p in g1.parents]
         assert sum(g1 is b for b in parent.blocks) == 1
 
         assert g1.parent_id is parent.name
@@ -47,48 +44,42 @@ def test_graph_inheritance():
     assert top.parent_id is None
     assert top.task_id is None
 
-    with checked_graph_instance(reip.Graph, top) as g1:
-        assert g1.task_id is None
-        with checked_graph_instance(reip.Graph, g1) as g2:
-            assert g1.task_id is None
-            with checked_graph_instance(reip.Graph, g2) as g3:
-                assert g1.task_id is None
-                with checked_graph_instance(reip.Graph, g3) as g4:
-                    assert g1.task_id is None
+    with checked_graph_instance(reip.Graph, top, [top]) as g1:
+        with checked_graph_instance(reip.Graph, g1, [g1, top]) as g2:
+            with checked_graph_instance(reip.Graph, g2, [g2, g1, top]) as g3:
+                with checked_graph_instance(reip.Graph, g3, [g3, g2, g1, top]) as g4:
+                    pass
                 assert reip.default_graph() is g3
-                with checked_graph_instance(reip.Graph, g3) as g5:
-                    assert g1.task_id is None
+                with checked_graph_instance(reip.Graph, g3, [g3, g2, g1, top]) as g5:
+                    pass
 
     top.clear()
 
 
 def test_task_inheritance():
     # test that a task cannot exist as a child of another task.
-    with checked_graph_instance(reip.Graph, reip.top_graph()) as g1:
+    top = reip.top_graph()
+    with checked_graph_instance(reip.Graph, top, [top]) as g1:
         assert g1.task_id is None
-        with checked_graph_instance(reip.Task, g1) as g2:
-            assert g2.task_id is g2.name
+        with checked_graph_instance(reip.Task, g1, [g1, top], task_id=False) as g2:
+            assert g2.task_id == g2.name
             with pytest.raises(RuntimeError):
                 with checked_graph_instance(reip.Task, g2):
                     pass
 
-            with checked_graph_instance(reip.Graph, g2) as g3:
-                assert g3.task_id is g2.name
-
-                with checked_graph_instance(reip.Graph, g3) as g4:
-                    assert g4.task_id is g2.name
-                    with checked_graph_instance(reip.Graph, g4) as g5:
-                        assert g5.task_id is g2.name
+            with checked_graph_instance(reip.Graph, g2, [g2, g1, top], task_id=g2.name) as g3:
+                with checked_graph_instance(reip.Graph, g3, [g3, g2, g1, top], task_id=g2.name) as g4:
+                    with checked_graph_instance(reip.Graph, g4, [g4, g3, g2, g1, top], task_id=g2.name) as g5:
+                        pass
                 assert reip.default_graph() is g3
 
                 with pytest.raises(RuntimeError):
                     with checked_graph_instance(reip.Task, g3):
                         pass
                 assert reip.default_graph() is g3
-                with checked_graph_instance(reip.Graph, g3) as g4:
-                    assert g4.task_id is g2.name
-                    with checked_graph_instance(reip.Graph, g4) as g5:
-                        assert g5.task_id is g2.name
+                with checked_graph_instance(reip.Graph, g3, [g3, g2, g1, top], task_id=g2.name) as g4:
+                    with checked_graph_instance(reip.Graph, g4, [g4, g3, g2, g1, top], task_id=g2.name) as g5:
+                        pass
 
     reip.top_graph().clear()
 
