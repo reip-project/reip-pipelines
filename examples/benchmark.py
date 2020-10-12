b3554a4f8fc441478c78ae8d40c35117604a1862
Bea Steers <bsteers@nyu.edu>
2020-10-11 23:45:41 -0400

    cleanup plotting

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -6,8 +6,20 @@ import numpy as np
 import reip
 import matplotlib.pyplot as plt
 
+plt.rc('font', family='serif')
+plt.rc('xtick', labelsize='x-small')
+plt.rc('ytick', labelsize='x-small')
 
-def run(duration=4):
+DIR = os.path.join(os.path.dirname(__file__), 'benchmark_results')
+RESULTS_FILE = os.path.join(DIR, '{}.json')
+PLT_FILE = os.path.join(DIR, '{}.png')
+DEFAULT_ID = 'serialization'
+
+MARKERS = 'ovs*Xd+'
+LINESTYLES = ['-', ':', '--', '-.']
+
+def run(duration=6, id=DEFAULT_ID):
+    os.makedirs(DIR, exist_ok=True)
     srcs = {
         # 'Thread': lambda sink: sink.gen_source(),
         'Pickle': lambda sink: sink.gen_source(task_id=1111, throughput='small'),
@@ -25,7 +37,7 @@ def run(duration=4):
         sink.spawn()
 
         results[name] = {}
-        for size in np.concatenate([np.arange(10, 100, 10), np.arange(100, 700, 50)]).astype(int):
+        for size in np.concatenate([np.arange(10, 100, 5), np.arange(100, 700, 50)]).astype(int):
             X = np.random.randn(size, size, size)
             gb = X.__sizeof__() / 8 / (1024**3)
 
@@ -50,32 +62,39 @@ def run(duration=4):
                 traceback.print_exc()
                 continue
 
-            results[name][gb] = gb * i / sw.total(name)
+            results[name][gb] = i / sw.total(name)
             print('{}: {:.4f}gb/s.  Buffer Size: {:>16} {:.8f}gb.  Throughput: {:.4f}gb.  Runtime: {:.2f}s, {:>7} iterations'.format(
                 name, results[name][gb], str(X.shape), gb, gb*i, sw.total(name), i))
         print()
 
         sink.join()
 
-    for name, res in results.items():
-        if name == 'thread':
-            continue
-        x, y = zip(*sorted(res.items()))
-        plt.plot(x, y, label=name)
-
-    plt.title('Throughput')
-    plt.legend()
-    plt.xlabel('Buffer Size (gb)')
-    plt.ylabel('Throughput Speed (gb/s)')
-    plt.xscale('log')
-    plt.yscale('log')
-    plt.savefig(os.path.join(os.path.dirname(__file__), 'benchmarks-serialization.png'))
-
-    with open(os.path.join(os.path.dirname(__file__), 'benchmarks-serialization.json'), 'w') as f:
+    _plot_results(results, id)
+    with open(RESULTS_FILE.format(id), 'w') as f:
         json.dump(results, f)
 
 
 
+def _plot_results(results, id=DEFAULT_ID):
+    plt.figure(figsize=(7, 3), dpi=300)
+    for (name, res), marker, ls in zip(results.items(), MARKERS, LINESTYLES):
+        x, y = zip(*sorted(((float(k), float(v)) for k, v in res.items())))
+        plt.plot(x, y, label=name, color='k', ls=ls)  # , marker=marker, markersize=4
+
+    plt.legend()
+    plt.xlabel('Buffer Size (gb)')
+    plt.ylabel('Speed (buffers/sec)')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title('Buffer throughput speeds across several serialization strategies.')
+    plt.tight_layout()
+    plt.savefig(PLT_FILE.format(id))
+
+def plot(id=DEFAULT_ID):
+    with open(RESULTS_FILE.format(id), 'r') as f:
+        results = json.load(f)
+    _plot_results(results, id)
+
 if __name__ == '__main__':
     import fire
-    fire.Fire(run)
+    fire.Fire()
