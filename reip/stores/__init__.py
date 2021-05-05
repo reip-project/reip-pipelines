6cc5afa24d5dd387d3f96a85306f15ca40da9839
Bea <bea.steers@gmail.com>
2021-05-05 18:22:34 -0400

    sketched global manager that could be used in pickled cases

--- reip/stores/__init__.py
+++ reip/stores/__init__.py
@@ -1,3 +1,29 @@
+
+class _Manager:
+    '''This manages a global manager object, and any namespaces from that manager.'''
+    def __init__(self):
+        self._manager = None
+        self.ns = {}
+
+    @property
+    def manager(self):
+        if self._manager is None:
+            import multiprocessing as mp
+            self._manager = mp.Manager()
+        return self._manager
+
+    def get(self, name):
+        if name is None:
+            return self.manager
+        try:
+            return self.ns[name]
+        except KeyError:
+            ns = self.ns[name] = self.manager
+            return ns
+
+_manager = _Manager()   
+get_manager = _manager.get
+
 from .core import *
 from .queue import *
 try:
