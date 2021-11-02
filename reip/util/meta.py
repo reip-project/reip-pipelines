70e6b19f97945f0c6accf301e982dd74aa0b4820
Bea <bea.steers@gmail.com>
2021-11-02 10:49:05 -0400

    add simpler meta that may be easier to get behind

---
+++ reip/util/meta.py
@@ -0,0 +1,118 @@
+class Meta(dict):
+    '''A metadata object. It combines a dictionary with metadata from earlier inputs.
+
+    This has two attributes 
+        - meta.data: a dictionary containing the values for this layer
+        - meta.inputs: a list of meta/dicts that represents the metadata 
+            from previous blocks
+    
+    The Meta object itself subclasses a dictionary which contains a flatten version
+    of meta.data and meta.inputs. Modifications to the meta object are applied to both
+    the flattened version and the data object. 
+    '''
+    def __init__(self, data=None, *inputs, deep_access=True, masked_keys=None):
+        # the current layer of data - a dict
+        self.data = data = data if data is not None else {}
+        # this meta object inputs - a list of meta objects
+        self.inputs = list(inputs)
+        self._deep_access = deep_access
+        self._masked_keys = set(masked_keys or ())
+        super().__init__(self._compile_dict())
+
+    def _compile_dict(self):
+        merged = {}
+        if self._deep_access:
+            for d in self.inputs[::-1]:
+                merged.update(d)
+            for k in self._masked_keys:
+                merged.pop(k, None)
+        merged.update(self.data)
+        return merged
+
+    def _compile(self):
+        return self.clear().update(self._compile_dict())
+
+    def __str__(self):
+        return 'Meta({}, [{}])'.format(self.data, ', '.join(map(str, self.inputs)))
+
+    def __getitem__(self, k):
+        if isinstance(k, (int, slice)):
+            return self.inputs[k]
+        return super().__getitem__(k)
+
+    def __setitem__(self, k, v):
+        if isinstance(k, (int, slice)):
+            self.inputs[k] = v
+            return
+        self.data[k] = v
+        return super().__setitem__(k, v)
+
+    def __delitem__(self, k):
+        if isinstance(k, (int, slice)):
+            del self.inputs[k]
+        if k in self.data:
+            del self.data[k]
+        return super().__delitem__(k)
+
+    def __or__(self, other):
+        return self.copy().update(other)
+
+    def __ior__(self, other):
+        return self.update(other)
+
+    def clear(self):
+        self.data.clear()
+        super().clear()
+        return self
+
+    def update(self, *a, **kw):
+        if a and not hasattr(a[0], '__len__'): # handle generators
+            a = (dict(a[0]),)
+        self.data.update(*a, **kw)
+        super().update(*a, **kw)
+        return self
+
+    def setdefault(self, k, v=None):
+        if k not in self:
+            self[k] = v
+        return self[k]
+
+    def pop(self, k, *default):
+        self.data.pop(k, None)
+        return super().pop(k, *default)
+
+    def copy(self):
+        return self.__class__({**self.data}, *self.inputs)
+
+    @classmethod
+    def as_meta(cls, d):
+        if not isinstance(d, cls):
+            if isinstance(d, Meta):
+                return cls(d, *d.inputs)
+            return cls(d)
+        return d
+
+    def append(self, *inputs):
+        self.inputs.extend(inputs)
+        self._compile()
+        return self
+
+    def extend(self, inputs):
+        self.inputs.extend(inputs)
+        self._compile()
+        return self
+
+
+class ImmutableMeta(Meta):
+    def __setitem__(self, k, v):
+        raise TypeError
+    def __delitem__(self, k):
+        raise TypeError
+    def update(self, *a, **kw):
+        raise TypeError
+    def setdefault(self, k, v=None):
+        raise TypeError
+    def append(self, *inputs):
+        raise TypeError
+    def extend(self, inputs):
+        raise TypeError
--- tests/test_meta.py
+++ tests/test_meta.py
@@ -5,16 +5,17 @@ import reip
 
 def test_meta():
     meta_a1 = reip.Meta({'i': 1, 'a': 1})
-    meta_a2 = reip.Meta({'i': 2, 'b': 2}, [meta_a1])
-    meta_a3 = reip.Meta({'i': 3, 'b': 3}, [meta_a2])
+    meta_a2 = reip.Meta({'i': 2, 'b': 2}, meta_a1)
+    meta_a3 = reip.Meta({'i': 3, 'b': 3}, meta_a2)
 
     meta_b1 = reip.Meta({'i': 1, 'a': 4})
-    meta_b2 = reip.Meta({'i': 2, 'c': 6}, [meta_b1])
+    meta_b2 = reip.Meta({'i': 2, 'c': 6}, meta_b1)
 
-    meta = reip.Meta({'i': 0, 'd': 10}, [meta_a3, meta_b2])
+    meta = reip.Meta({'i': 0, 'd': 10}, meta_a3, meta_b2)
     print(dict(meta))
 
-    flat = {'i': 0, 'd': 10, 'b': 3, 'c': 6, 'a': 4}
+    # flat = {'i': 0, 'd': 10, 'b': 3, 'c': 6, 'a': 4}
+    flat = {'i': 0, 'd': 10, 'b': 3, 'c': 6, 'a': 1}
 
     # check as an iterable
     assert dict(meta) == flat
@@ -26,12 +27,12 @@ def test_meta():
     assert 'z' not in meta
 
     # check getitem
-    assert meta['a'] == 4
+    assert meta['a'] == 1
     assert meta['b'] == 3
     assert meta['c'] == 6
     assert meta['d'] == 10
     assert meta['i'] == 0
-    assert meta.get('a', 99) == 4
+    assert meta.get('a', 99) == 1
     assert meta.get('z', 99) == 99
 
     # check slicing input list
@@ -43,20 +44,20 @@ def test_meta():
     assert set(meta.values()) == set(flat.values())
     assert set(meta.items()) == set(flat.items())
 
-    # check iterators
-    depths = [
-        [meta.data],
-        [meta_a3.data, meta_b2.data],
-        [meta_a2.data, meta_b1.data],
-        [meta_a1.data],
-    ]
+    # # check iterators
+    # depths = [
+    #     [meta.data],
+    #     [meta_a3.data, meta_b2.data],
+    #     [meta_a2.data, meta_b1.data],
+    #     [meta_a1.data],
+    # ]
 
-    assert list(meta.depths()) == depths
-    assert list(meta.nearfirst()) == [d for ds in depths for d in ds]
-    assert list(meta.deepfirst()) == [
-        meta.data, meta_a3.data, meta_a2.data, meta_a1.data, 
-        meta_b2.data, meta_b1.data
-    ]
+    # assert list(meta.depths()) == depths
+    # assert list(meta.nearfirst()) == [d for ds in depths for d in ds]
+    # assert list(meta.deepfirst()) == [
+    #     meta.data, meta_a3.data, meta_a2.data, meta_a1.data, 
+    #     meta_b2.data, meta_b1.data
+    # ]
 
     # check set/del item
     meta['b'] = 5
@@ -65,9 +66,12 @@ def test_meta():
     assert 'b' in meta.data
     del meta['b']
     assert 'b' not in meta.data
-    assert 'b' in meta.deleted_keys
+    assert 'b' not in meta
+    # assert 'b' in meta.deleted_keys
     meta['b'] = 6
-    assert 'b' not in meta.deleted_keys
+    assert 'b' in meta
+    assert 'b' in meta.data
+    # assert 'b' not in meta.deleted_keys
     assert meta['b'] == 6
 
     # check append, extend, update
@@ -83,7 +87,7 @@ def test_meta():
     assert meta['g'] == 12
     assert meta['h'] == 13
 
-    assert meta.find_key('a') is meta_b1.data
-    meta.scrub_key('b')
-    for m in meta.nearfirst():
-        assert 'b' not in m
+    # assert meta.find_key('a') is meta_b1.data
+    # meta.scrub_key('b')
+    # for m in meta.nearfirst():
+    #     assert 'b' not in m
