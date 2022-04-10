241e9b26e4498afd8dcf88b6e3a23161b85b0f6d
Bea <bea.steers@gmail.com>
2022-04-10 17:00:12 -0400

    update encryption to newer non-deprecated package

--- reip/blocks/encrypt.py
+++ reip/blocks/encrypt.py
@@ -3,8 +3,9 @@ import io
 import base64
 import tarfile
 from Crypto import Random
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
+from Crypto.Util import Padding
 
 import reip
 
@@ -13,19 +14,24 @@ class TwoStageEncrypt(reip.Block):
     '''Encrypt a file with a fresh generated key, then encrypt the key with
     the root key. Then tar the encrypted file and the encrypted key together.
     '''
-    PADDING = b'{'
+    #PADDING = b'{'
     BLOCK_SIZE = 32
 
-    def __init__(self, filename, rsa_key, create=True, remove_files=False, **kw):
+    def __init__(self, filename, rsa_key, remove_files=False, extra_files=None, is_rsa_key_file=True, **kw):
         super().__init__(**kw)
         self.filename = str(filename)
         self.remove_files = remove_files
-        self.public_key = rsa_key
         self.gz = self.filename.endswith('.gz')
-        if not os.path.isfile(self.public_key):
-            if not create:
-                raise OSError('Public key "{}" not found.'.format(self.public_key))
-            create_rsa(self.public_key)
+        self.extra_files = extra_files
+
+        if is_rsa_key_file:
+            if not os.path.isfile(rsa_key):
+                raise OSError("No rsa key found.")
+            with open(rsa_key, 'r') as f:
+                rsa_key = f.read()
+        self.public_key = RSA.importKey(str(rsa_key))
+        self.cipher = PKCS1_OAEP.new(self.public_key)
+
 
     def process(self, file, meta):
         '''Encrypt file with AES 4096.
@@ -35,24 +41,34 @@ class TwoStageEncrypt(reip.Block):
             default: current dir
         '''
         fname = reip.util.ensure_dir(self.filename.format(
-            name=reip.util.fname(file), **meta))
-        compressed = self.compress(self.encrypt(file), fname)
+            name=reip.util.fname(file), fname=os.path.basename(file), **meta))
+
+        with open(file, 'rb') as f:
+            enc_message = f.read()
+
+        enc = self.encrypt(enc_message, os.path.basename(file))
+
+        extra_files = reip.util.resolve_call(self.extra_files, file, meta=meta) or {}
+        enc.update(extra_files)
+
+        compressed = self.compress(enc, fname)
         self.maybe_remove_files(file)
         return [compressed], {}
 
-    def encrypt(self, file):
+    def encrypt(self, msg, name='file'):
         # Encrypt the file using AES and AES using RSA
         aes_key = Random.new().read(AES.key_size[2])
-        with open(file, 'rb') as f:
-            enc_message = base64.b64encode(
-                AES.new(aes_key).encrypt(self.pad(f.read())))
+        key = AES.new(aes_key, AES.MODE_CBC)
+
+        msg = Padding.pad(msg, AES.block_size)
+        msg = key.iv + key.encrypt(msg)
+        msg = base64.b64encode(msg)
+
         # gather files
-        fbase = os.path.basename(file)
-        files = {
-            fbase + '.enc': enc_message,
-            fbase + '.key': load_rsa(self.public_key).encrypt(aes_key, 32)[0]
+        return {
+            name+'.enc': msg,
+            name+'.key': self.cipher.encrypt(aes_key)
         }
-        return files
 
     def compress(self, files, out_file):
         # write tar
@@ -66,40 +82,48 @@ class TwoStageEncrypt(reip.Block):
             for f in files:
                 os.remove(f)
 
-    def pad(self, msg):
-        n = self.BLOCK_SIZE - len(msg) % self.BLOCK_SIZE
-        return msg + n * self.PADDING
-
-    def make_decrypt(self, filename):
-        return TwoStageDecrypt(filename, public2private(self.public_key))
-
 
 class TwoStageDecrypt(reip.Block):
     '''Decrypt a file encrypted with TwoStepEncryptFile.
     '''
+    PADDING = b'{'
+    BLOCK_SIZE = 32
+
     def __init__(self, filename, rsa_key, remove_files=False, **kw):
         super().__init__(**kw)
         self.filename = str(filename)
-        self.private_key = rsa_key
         self.remove_files = remove_files
+
+        if os.path.isfile(rsa_key):
+            with open(rsa_key, 'r') as f:
+                rsa_key = f.read()
+        self.private_key = RSA.importKey(rsa_key)
+        self.cipher = PKCS1_OAEP.new(self.private_key)
 
     def process(self, file, meta=None):
         # get the output filename and make sure the parent dir exists
         fname = reip.util.ensure_dir(self.filename.format(
             name=reip.util.fname(file), **meta))
         # decrypt file to disk
-        enc_file, enc_key = self.decompress(file)
-        decrypted = self.decrypt(enc_file, enc_key, fname)
-        self.maybe_remove_files(file, enc_file, enc_key)
-        return [decrypted], {}
-
-    def decrypt(self, enc_file, enc_key, fname):
-        '''Decrypt file with AES 4096.'''
+        enc_msg, enc_key = self.decompress(file)
+        decrypted = self.decrypt(enc_msg, enc_key)
         with open(fname, 'wb') as f:
-            f.write(
-                AES.new(load_rsa(self.private_key).decrypt(enc_key)).decrypt(
-                    base64.b64decode(enc_file)).rstrip(b'{'))
-        return fname
+            f.write(decrypted)
+
+        self.maybe_remove_files(file)
+        return [fname], {}
+
+    def decrypt(self, msg, enc_key):
+        '''Decrypt file with AES 4096.'''
+        msg = base64.b64decode(msg)
+
+        iv, msg = msg[:AES.block_size], msg[AES.block_size:]
+        aes_key = self.cipher.decrypt(enc_key)
+        key = AES.new(aes_key, AES.MODE_CBC, iv)
+
+        msg = key.decrypt(msg)
+        msg = Padding.unpad(msg, AES.block_size)
+        return msg
 
     def decompress(self, filename):
         # write file and key to tar file
@@ -125,25 +149,28 @@ def get_private_key(public_fname):
 def public2private(public_fname):
     return '{}_private{}'.format(*os.path.splitext(public_fname))
 
+def private2public(private_fname):
+    return '{}.pub'.format(private_fname)
+
 def load_rsa(rsa_key):
     with open(rsa_key, 'rb') as f:
         return RSA.importKey(f.read())
 
-def create_rsa(public_fname, private_fname=False, bits=2048):
+def create_rsa(private_fname=None, public_fname=None, bits=2048):
     private_key = RSA.generate(bits, e=65537)
     public_key = private_key.publickey()
 
+    if not (private_fname or public_fname):
+        private_fname = os.path.expanduser('~/.ssh/encrypt_rsa')
     private_fname = private_fname or public2private(public_fname)
-    print('Creating RSA Key Pair:')
-    print('\tprivate:', private_fname)
-    print('\tpublic:', public_fname)
+    public_fname = public_fname or private2public(private_fname)
 
     reip.util.ensure_dir(public_fname)
     with open(public_fname, 'wb') as f:
         f.write(public_key.exportKey("PEM"))
     with open(private_fname, 'wb') as f:
         f.write(private_key.exportKey("PEM"))
-    return private_fname, private_fname
+    return private_fname, public_fname
 
 def tar_addbytes(tar, f, data):
     t = tarfile.TarInfo(f)
